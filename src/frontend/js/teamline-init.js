85cc913fa5752be4ca8fc08881c0a5247fe42978
Felix Grund <felix.grund@scandio.de>
2017-04-10 19:19:43 -0700

    further major improvements

--- src/frontend/js/teamline-init.js
+++ src/frontend/js/teamline-init.js
@@ -1,11 +1,16 @@
 (function() {
 
+	var currentTeamName;
+	var teamData = {};
+
 	var settings = {
+		chartSelector: '#teamline-chart svg',
 		width: 700,
-		height: 700,
+		height: 800,
 		margin: {top: 0, right: 60, bottom: 50, left: 100},
 		legendMargin: {top: 10, right: 80, left: 0, bottom: 30},
-		marginTopToXAxis: 80 // TODO: don't know how to calculate this
+		marginTopToXAxis: 80, // TODO: don't know how to calculate this
+		tooltipDateFormat: 'MMMM Do YYYY, h:mm:ss a'
 	};
 
 	var props = {
@@ -23,17 +28,23 @@
 		}
 	};
 
+	var templates = {
+		tooltip: Handlebars.compile($("#tooltip-template").html()),
+		buttons: Handlebars.compile($("#buttons-template").html())
+	};
+
 	function createChartData(type, userNames, userIndex, contributionArray) {
 		var userName = userNames[userIndex];
 		var values = $.map(contributionArray, function(commit) {
-			return {
+			var commitClone = $.extend({}, commit);
+			return $.extend(commitClone, {
 				timestamp: commit.timestamp,
 				passCountAccumulated: commit.passCountAccumulated,
 				coverageContribAccumulated: commit.coverageContribAccumulated,
 				userIndex: userIndex,
 				userName: userName,
 				type: type
-			};
+			});
 		});
 		return {
 			key: props[type].label(userName),
@@ -54,10 +65,31 @@
 		});
 	}
 
+	function tooltip(object) {
+		var point = object.point;
+		var items = [
+			{ label: 'Time', value: moment(point.timestamp).format(settings.tooltipDateFormat) },
+			{ label: 'Grade', value: point.grade },
+			{ label: 'Passed Tests', value: point.passCount },
+			{ label: 'Failed Tests', value: point.failCount },
+			{ label: 'Skipped Tests', value: point.skipCount },
+			{ label: 'Coverage', value: point.coverage }
+		];
+		return templates.tooltip({ items: items });
+	}
 
-	d3.json("data/team78.json", function(error, data) {
+	function addButtons(deliverables) {
+		var buttons = [];
+		$.each(Object.keys(deliverables).sort(), function(index, key) {
+			buttons.push({key: key, label: key.toUpperCase(), classes: index === 0 ? 'active' : ''});
+		});
+		var renderedTemplate = templates.buttons({buttons: buttons});
+		$('#teamline-buttons').append(renderedTemplate);
+	}
 
-		var targetDeliverable = data.deliverables.d1;
+	function drawChart(deliverableName) {
+		var data = teamData[currentTeamName];
+		var targetDeliverable = data.deliverables[deliverableName];
 		var usersCommitData = targetDeliverable.users;
 		var userNames = Object.keys(usersCommitData).sort();
 		var user1Data = usersCommitData[userNames[0]].beforeDeadline;
@@ -80,6 +112,8 @@
 			return commit.timestamp;
 		};
 
+		$(settings.chartSelector).html('');
+
 		addAccumulatedData(user1Data.commits);
 		addAccumulatedData(user2Data.commits);
 
@@ -95,16 +129,14 @@
 		};
 
 		nv.addGraph(function() {
-			var WIDTH = 700;
-			var HEIGHT = 700;
-
 			var chart = nv.models.lineChart()
-				.width(WIDTH)
-				.height(HEIGHT)
+				.width(settings.width)
+				.height(settings.height)
 				.margin(settings.margin)
-				.x(x).y(y);
+				.x(x).y(y)
+				.tooltipContent(tooltip);
 
-			var d3Obj = d3.select('#chart svg').datum([
+			var d3Obj = d3.select(settings.chartSelector).datum([
 				chartData.user1.passRate, chartData.user2.passRate,
 				chartData.user1.coverage, chartData.user2.coverage
 			]);
@@ -115,33 +147,46 @@
 			});
 
 			chart.yAxis.tickFormat(function(timestamp) {
-				return d3.time.format('%x')(new Date(timestamp))
+				return d3.time.format('%a, %b %d')(new Date(timestamp))
 			});
 
 			chart.forceX([-100,100]);
-			// 1209600000 = 2 weeks in ms; 86400000 = 1 day in ms
-			chart.forceY([targetDeliverable.due-1209600000, targetDeliverable.due+86400000]);
+			// 1209600000 = 2 weeks in ms; 172800000 = 1 day in ms
+			chart.forceY([targetDeliverable.due-1209600000, targetDeliverable.due+172800000]);
 			chart.legend.margin(settings.legendMargin);
 
 			d3Obj.style({width: settings.width, height: settings.height}).call(chart);
-
-			// d3.select('#chart svg').append('line')
-			// 	.attr({
-			// 		x1: 0,
-			// 		x2: WIDTH,
-			// 		y1: settings.marginTopToXAxis + chart.yAxis.scale()(targetDeliverable.due),
-			// 		y2: settings.marginTopToXAxis + chart.yAxis.scale()(targetDeliverable.due)
-			// 	})
-			// 	.attr('class', 'duedate-line');
-
-			//d3.select('.nv-legendWrap').attr('transform', 'translate(0, 100)');
-
 
 			window.chart = chart;
 			nv.utils.windowResize(chart.update);
 
 		});
+	}
 
+	function loadData(callback) {
+		d3.json('data/'+currentTeamName+'.json', function(error, data) {
+			teamData[currentTeamName] = data;
+			addButtons(data.deliverables);
+			callback();
+		});
+	}
+
+	function initialize() {
+		currentTeamName = 'team78';
+		loadData(function() {
+			drawChart('d1');
+		});
+	}
+
+	function onButtonClick(e) {
+		var $button = $(this);
+		$button.addClass('active').siblings().removeClass('active');
+		drawChart($button.data('key'));
+	}
+
+	$(document.ready).ready(function() {
+		initialize();
+		$('#teamline-buttons').on('click', 'button', onButtonClick);
 	});
 
 }());
